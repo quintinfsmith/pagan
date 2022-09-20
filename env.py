ed2f561b5d0759af4d5d94ed0ecd6a9622b697b9
Quintin Smith <smith.quintin@protonmail.com>
2022-09-20 09:28:08 -0700

    work on user environment

--- env.py
+++ env.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import os
 import re
+import time
 
 from typing import Optional
 import wrecked
@@ -11,13 +12,18 @@ class InvalidCursor(Exception):
     '''Raised when attempting to pass a cursor without enough arguments'''
 
 class NoterEnvironment:
+    tick_delay = 60 / 24
+
     def __init__(self):
         self.root = wrecked.init()
         self.channel_rects = [[] for i in range(16)]
         self.opus_manager = OpusManager()
 
-        self.rendered_channel_rects = []
-        self.beat_sizes = []
+        self.rendered_channel_rects = set()
+        self.rendered_beat_widths = {}
+
+    def kill(self):
+        wrecked.kill()
 
     def load(self, path: str) -> None:
         if os.path.isdir(path):
@@ -31,6 +37,58 @@ class NoterEnvironment:
             for i, grouping in enumerate(channel):
                 grouping_rect = self.root.new_rect()
                 self.channel_rects[c].append(grouping_rect)
+
+    def tick(self):
+        channels = self.opus_manager.channel_groupings
+        flag_draw = False
+        line_position = 0
+        for c in self.opus_manager.channel_order:
+            for i, line in enumerate(channels[c]):
+                if (c, i) in self.rendered_channel_rects:
+                    line_position += 1
+                    continue
+
+                self.rendered_channel_rects.add((c,i))
+                rect_line = self.channel_rects[c][i]
+                rect_line.clear_children()
+                running_len = 0
+                split_positions = [0]
+                for b, beat in enumerate(line):
+                    bstring = beat.to_string()
+                    rect_beat = rect_line.new_rect()
+                    new_width = max(len(bstring), self.rendered_beat_widths.get(b,0))
+                    rect_beat.resize(new_width, 1)
+                    rect_beat.move(running_len + 1, 0)
+                    rect_beat.set_string(0,0,bstring)
+                    running_len += new_width + 1
+
+                    split_positions.append(running_len)
+
+                rect_line.resize(running_len + 1, 1)
+                rect_line.move(4, line_position)
+                rect_line.set_fg_color(wrecked.BLUE)
+                for x in split_positions:
+                    rect_line.set_string(x, 0, "|")
+
+                self.root.set_string(0, line_position, f"{c}:{i} ")
+
+                flag_draw = True
+                line_position += 1
+
+        # Draw cursor
+
+
+        if flag_draw:
+            self.root.draw()
+
+    def run(self):
+        self.running = True
+        while self.running:
+            try:
+                self.tick()
+                time.sleep(self.tick_delay)
+            except KeyboardInterrupt:
+                self.running = False
 
 class OpusManager:
     BASE = 12
@@ -121,7 +179,7 @@ class OpusManager:
         return output
 
     def load_folder(self, path: str) -> None:
-        base = 12
+        base = self.BASE
 
         channel_map = {}
         suffix_patt = re.compile(".*_(?P<suffix>([0-9A-Z]{1,3})?)(\..*)?", re.I)
@@ -136,12 +194,14 @@ class OpusManager:
             channel = None
             for hit in suffix_patt.finditer(filename):
                 channel = int(hit.group('suffix'), 16)
+
             if channel is not None:
                 channel_map[filename] = channel
                 filenames_clean.append(filename)
 
         beat_count = 1
         for filename in filenames_clean:
+            channel = channel_map[filename]
             content = ""
             with open(f"{path}/{filename}", 'r') as fp:
                 content = fp.read()
@@ -152,13 +212,16 @@ class OpusManager:
                     chunks[i] = f"[{chunk}"
 
             for x, chunk in enumerate(chunks):
-                grouping = MGrouping.from_string(chunk, base=base, channel=channel_map[filename])
-                beat_count = max(len(grouping), beat_count)
+                grouping = MGrouping.from_string(chunk, base=base, channel=channel)
                 if grouping:
+                    beat_count = max(len(grouping), beat_count)
                     grouping.set_size(beat_count, True)
                     grouping.crop_redundancies()
 
-                    self.channel_groupings[channel_map[filename]].append(grouping)
+                    self.channel_groupings[channel].append(grouping)
+
+        self.opus_beat_count = beat_count
+
 
     def load_file(self, path: str) -> MGrouping:
         base = 12
@@ -172,21 +235,22 @@ class OpusManager:
             if i > 0:
                 chunks[i] = f"[{chunk}"
 
+        self.opus_beat_count = 1
         for x, chunk in enumerate(chunks):
             grouping = MGrouping.from_string(chunk, base=base, channel=x)
             self.channel_groupings[x].append(grouping)
+            self.opus_beat_count = max(self.opus_beat_count, len(grouping))
 
     def import_midi(self, path: str) -> None:
         midi = MIDI.load(path)
         opus = MGrouping.from_midi(midi)
         tracks = opus.split(split_by_channel)
 
+        self.opus_beat_count = 1
         for i, mgrouping in enumerate(tracks):
             for _j, split_line in enumerate(mgrouping.split(split_by_note_order)):
                 self.channel_groupings[i].append(split_line)
-
-    def kill(self):
-        wrecked.kill()
+                self.opus_beat_count = max(self.opus_beat_count, len(split_line))
 
 def split_by_channel(event, other_events):
     return event[3]

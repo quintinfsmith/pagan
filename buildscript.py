cee5f3cdff3c03cc1f7d1fd8ef34f589ba7c2049
Quintin Smith <smith.quintin@protonmail.com>
2022-10-19 09:58:56 -0700

    Name Change

--- buildscript.py
+++ buildscript.py
@@ -7,14 +7,14 @@ file_path = os.path.realpath(__file__)
 os.chdir(file_path[0:file_path.rfind("/") + 1])
 
 
-os.system("cp src radixal -rf")
+os.system("cp src radixulous -rf")
 if "--local" in sys.argv:
     os.system("python3 setup.py install --prefix ~/.local/")
 elif "--publish" in sys.argv:
     os.system("python3 setup.py sdist bdist_wheel")
     os.system("python3 -m twine upload dist/*")
 
-os.system("rm radixal.egg-info -rf")
+os.system("rm radixulous.egg-info -rf")
 os.system("rm build -rf")
 os.system("rm dist -rf")
-os.system("rm radixal -rf")
+os.system("rm radixulous -rf")

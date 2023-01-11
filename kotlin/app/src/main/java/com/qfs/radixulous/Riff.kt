c877cdd7d1ff00e51dbc483dfb229c5bb88283aa
Quintin Smith <smith.quintin@protonmail.com>
2023-01-10 23:09:40 -0800

    kotlin; defining generators in soundfonts

--- kotlin/app/src/main/java/com/qfs/radixulous/Riff.kt
+++ kotlin/app/src/main/java/com/qfs/radixulous/Riff.kt
@@ -18,9 +18,10 @@ class SoundFont {
 
     // Populated by sdta
     // NOTE: smpl size needs to be 2 * sm24 size
-    var sampleData: ByteArray = ByteArray(0)
-
+    var instruments: MutableList<Instrument> = mutableListOf()
     var presets: MutableList<Preset> = mutableListOf()
+    var samples: MutableList<Sample> = mutableListOf()
+    var sampleData: ByteArray = ByteArray(0)
 
     constructor(riff: Riff) {
         var tmp_sample_a: ByteArray? = null
@@ -97,8 +98,10 @@ class SoundFont {
         }
 
         // Merge sample data and convert to big-endian
+        var sample_word_size = 2
         if (tmp_sample_a != null) {
             if (tmp_sample_b != null) {
+                sample_word_size = 3
                 this.sampleData = ByteArray(tmp_sample_a!!.size + tmp_sample_b!!.size)
                 for (i in 0 until tmp_sample_a!!.size) {
                     this.sampleData[(i * 3)] = tmp_sample_a!![(2 * i) + 1]
@@ -124,49 +127,143 @@ class SoundFont {
             pdta_map[sub_chunk.type] = sub_chunk
         }
 
-        var pgenerators: MutableList<Triple<Int, Int, Int>> = mutableListOf()
-        for (i in 0 until pdta_map["pgen"]!!.bytes.size / 4) {
-            pgenerators.add(
-                Triple(
-                    pdta_map["pgen"]!!.bytes[(i * 4)].toInt() + (pdta_map["pgen"]!!.bytes[(i * 4) + 1].toInt() * 256),
-                    pdta_map["pgen"]!!.bytes[(i * 4) + 2].toInt(),
-                    pdta_map["pgen"]!!.bytes[(i * 4) + 3].toInt()
+        var ibag_bytes = pdta_map["ibag"]!!.bytes
+        var inst_bytes = pdta_map["inst"]!!.bytes
+        var shdr_bytes = pdta_map["shdr"]!!.bytes
+
+        for (i in 0 until shdr_bytes.size / 46) {
+            var offset = i * 46
+            var sample_name = ""
+            for (j in 0 until 20) {
+                var b = shdr_bytes[offset + j].toInt()
+                if (b == 0) {
+                    break
+                }
+                sample_name = "$sample_name${b.toChar()}"
+            }
+
+            this.samples.add(
+                Sample(
+                    sample_name,
+                    shdr_bytes[offset + 20].toInt()
+                        + (shdr_bytes[offset + 21].toInt() * 256)
+                        + (shdr_bytes[offset + 22].toInt() * (256 * 256))
+                        + (shdr_bytes[offset + 23].toInt() * (256 * 256 * 256)),
+                    shdr_bytes[offset + 24].toInt()
+                        + (shdr_bytes[offset + 25].toInt() * 256)
+                        + (shdr_bytes[offset + 26].toInt() * (256 * 256))
+                        + (shdr_bytes[offset + 27].toInt() * (256 * 256 * 256)),
+                    shdr_bytes[offset + 28].toInt()
+                        + (shdr_bytes[offset + 29].toInt() * 256)
+                        + (shdr_bytes[offset + 30].toInt() * (256 * 256))
+                        + (shdr_bytes[offset + 31].toInt() * (256 * 256 * 256)),
+                    shdr_bytes[offset + 32].toInt()
+                        + (shdr_bytes[offset + 33].toInt() * 256)
+                        + (shdr_bytes[offset + 34].toInt() * (256 * 256))
+                        + (shdr_bytes[offset + 35].toInt() * (256 * 256 * 256)),
+                    shdr_bytes[offset + 36].toInt()
+                        + (shdr_bytes[offset + 37].toInt() * 256)
+                        + (shdr_bytes[offset + 38].toInt() * (256 * 256))
+                        + (shdr_bytes[offset + 39].toInt() * (256 * 256 * 256)),
+                    shdr_bytes[offset + 40].toInt(),
+                    shdr_bytes[offset + 41].toInt(),
+                    shdr_bytes[offset + 42].toInt() + (shdr_bytes[offset + 43].toInt() * 256),
+                    shdr_bytes[offset + 44].toInt() + (shdr_bytes[offset + 45].toInt() * 256)
                 )
             )
         }
-        var pmodulators: MutableList<Modulator> = mutableListOf()
-        var pmod_bytes = pdta_map["pmod"]!!.bytes
-        for (i in 0 until pmod_bytes.size / 10) {
-            pmodulators.add(
-                Modulator(
-                    pmod_bytes[(i * 10)].toInt() + (pmod_bytes[(i * 10) + 1].toInt() * 256),
-                    pmod_bytes[(i * 10) + 2].toInt() + (pmod_bytes[(i * 10) + 3].toInt() * 256),
-                    pmod_bytes[(i * 10) + 4].toInt() + (pmod_bytes[(i * 10) + 5].toInt() * 256),
-                    pmod_bytes[(i * 10) + 6].toInt() + (pmod_bytes[(i * 10) + 7].toInt() * 256),
-                    pmod_bytes[(i * 10) + 8].toInt() + (pmod_bytes[(i * 10) + 9].toInt() * 256)
+
+        // TODO: Verify INST chunk (mod 22 == 0, min size 44, and exists)
+        var instrument_count = pdta_map["inst"]!!.bytes.size / 22
+        var ibag_entry_size = 4
+
+        var instrument_list_indices: MutableList<Pair<Int, Int>> = mutableListOf()
+        for (i in 0 until instrument_count) {
+            var ibag_index = inst_bytes[(i * ibag_entry_size)] + (inst_bytes[(i * ibag_entry_size) + 1] * 256)
+            var offset = i * ibag_entry_size
+            instrument_list_indices.add(
+                Pair(
+                    ibag_bytes[offset + 0] + (ibag_bytes[offset + 1] * 256),
+                    ibag_bytes[offset + 2] + (ibag_bytes[offset + 3] * 256)
                 )
             )
         }
-        var igenerators: MutableList<Triple<Int, Int, Int>> = mutableListOf()
+
+        var igenerators: MutableList<Generator> = mutableListOf()
         for (i in 0 until pdta_map["igen"]!!.bytes.size / 4) {
+            var offset = i * 4
             igenerators.add(
-                Triple(
-                    pdta_map["igen"]!!.bytes[(i * 4)].toInt() + (pdta_map["igen"]!!.bytes[(i * 4) + 1].toInt() * 256),
-                    pdta_map["igen"]!!.bytes[(i * 4) + 2].toInt(),
-                    pdta_map["igen"]!!.bytes[(i * 4) + 3].toInt()
+                Generator(
+                    pdta_map["igen"]!!.bytes[offset].toInt() + (pdta_map["igen"]!!.bytes[offset + 1].toInt() * 256),
+                    pdta_map["igen"]!!.bytes[offset + 2].toInt(),
+                    pdta_map["igen"]!!.bytes[offset + 3].toInt()
                 )
             )
         }
         var imodulators: MutableList<Modulator> = mutableListOf()
         var imod_bytes = pdta_map["imod"]!!.bytes
         for (i in 0 until imod_bytes.size / 10) {
+            var offset = i * 10
             imodulators.add(
                 Modulator(
-                    imod_bytes[(i * 10)].toInt() + (imod_bytes[(i * 10) + 1].toInt() * 256),
-                    imod_bytes[(i * 10) + 2].toInt() + (imod_bytes[(i * 10) + 3].toInt() * 256),
-                    imod_bytes[(i * 10) + 4].toInt() + (imod_bytes[(i * 10) + 5].toInt() * 256),
-                    imod_bytes[(i * 10) + 6].toInt() + (imod_bytes[(i * 10) + 7].toInt() * 256),
-                    imod_bytes[(i * 10) + 8].toInt() + (imod_bytes[(i * 10) + 9].toInt() * 256)
+                    imod_bytes[offset + 0].toInt() + (imod_bytes[offset + 1].toInt() * 256),
+                    imod_bytes[offset + 2].toInt() + (imod_bytes[offset + 3].toInt() * 256),
+                    imod_bytes[offset + 4].toInt() + (imod_bytes[offset + 5].toInt() * 256),
+                    imod_bytes[offset + 6].toInt() + (imod_bytes[offset + 7].toInt() * 256),
+                    imod_bytes[offset + 8].toInt() + (imod_bytes[offset + 9].toInt() * 256)
+                )
+            )
+        }
+
+        // Instruments
+        for (i in 0 until instrument_count - 1) {
+            var instrument_name = ""
+            for (j in 0 until 20) {
+                var b = inst_bytes[(i * 22) + j].toInt()
+                if (b == 0) {
+                    break
+                }
+                instrument_name = "$instrument_name${b.toChar()}"
+            }
+
+            var instrument = Instrument(instrument_name)
+            // Generators
+            var generators_to_use: MutableList<Generator> = mutableListOf()
+
+            for (j in instrument_list_indices[i].first until instrument_list_indices[i + 1].first) {
+                generators_to_use.add(igenerators[j])
+            }
+            this.generate_instrument(instrument, generators_to_use)
+
+            // Modulators
+            for (j in instrument_list_indices[i].second until instrument_list_indices[i + 1].second) {
+                this.modulate(instrument, imodulators[j])
+            }
+        }
+
+
+        var pgenerators: MutableList<Generator> = mutableListOf()
+        for (i in 0 until pdta_map["pgen"]!!.bytes.size / 4) {
+            var offset = i * 4
+            pgenerators.add(
+                Generator(
+                    pdta_map["pgen"]!!.bytes[offset + 0].toInt() + (pdta_map["pgen"]!!.bytes[offset + 1].toInt() * 256),
+                    pdta_map["pgen"]!!.bytes[offset + 2].toInt(),
+                    pdta_map["pgen"]!!.bytes[offset + 3].toInt()
+                )
+            )
+        }
+        var pmodulators: MutableList<Modulator> = mutableListOf()
+        var pmod_bytes = pdta_map["pmod"]!!.bytes
+        for (i in 0 until pmod_bytes.size / 10) {
+            var offset = i * 10
+            pmodulators.add(
+                Modulator(
+                    pmod_bytes[offset + 0].toInt() + (pmod_bytes[offset + 1].toInt() * 256),
+                    pmod_bytes[offset + 2].toInt() + (pmod_bytes[offset + 3].toInt() * 256),
+                    pmod_bytes[offset + 4].toInt() + (pmod_bytes[offset + 5].toInt() * 256),
+                    pmod_bytes[offset + 6].toInt() + (pmod_bytes[offset + 7].toInt() * 256),
+                    pmod_bytes[offset + 8].toInt() + (pmod_bytes[offset + 9].toInt() * 256)
                 )
             )
         }
@@ -174,55 +271,41 @@ class SoundFont {
 
         var preset_count = pdta_map["phdr"]!!.bytes.size / 38
         var pbag_entry_size = 4
-        var ibag_entry_size = 4
 
-        var list_indices: MutableList<Array<Int>> = mutableListOf()
-        for (i in 0 until preset_count - 1) {
+        var preset_list_indices: MutableList<Pair<Int, Int>> = mutableListOf()
+        var pbag_bytes = pdta_map["pbag"]!!.bytes
+        for (i in 0 until preset_count) {
             var wPresetBagIndex = pdta_map["phdr"]!!.bytes[(i * 38) + 24] + (pdta_map["phdr"]!!.bytes[(i * 38) + 25] * 256)
-            list_indices.add(
-                pdta_map["pbag"]!!.bytes[(wPresetBagIndex * pbag_entry_size)]
-                    + (pdta_map["pbag"]!!.bytes[(wPresetBagIndex * pbag_entry_size) + 1] * 256),
-                pdta_map["pbag"]!!.bytes[(wPresetBagIndex * pbag_entry_size) + 2]
-                    + (pdta_map["pbag"]!!.bytes[(wPresetBagIndex * pbag_entry_size) + 3] * 256),
-                pdta_map["ibag"]!!.bytes[(i * ibag_entry_size)]
-                    + (pdta_map["ibag"]!!.bytes[(i * ibag_entry_size) + 1] * 256),
-                pdta_map["ibag"]!!.bytes[(i * ibag_entry_size) + 2]
-                    + (pdta_map["ibag"]!!.bytes[(i * ibag_entry_size) + 3] * 256)
+            var offset = wPresetBagIndex * pbag_entry_size
+            preset_list_indices.add(
+                Pair(
+                    pbag_bytes[offset + 0].toInt() + (pbag_bytes[offset + 1] * 256).toInt(),
+                    pbag_bytes[offset + 2].toInt() + (pbag_bytes[offset + 3] * 256).toInt()
+                )
             )
         }
-        list_indices.add(
-            pdta_map["pgen"]!!.bytes.size / 4,
-            pdta_map["pmod"]!!.bytes.size / 10,
-            pdta_map["igen"]!!.bytes.size / 4,
-            pdta_map["imod"]!!.bytes.size / 10
-        )
-
 
         for (i in 0 until preset_count - 1) {
-            var wGenNdx = list_indices[i][0]
-            var wModNdx = list_indices[i][1]
-            var wInstGenNdx = list_indices[i][2]
-            var wInstModNdx = list_indices[i][3]
+            var wGenNdx = preset_list_indices[i].first
+            var wModNdx = preset_list_indices[i].second
 
-            var wGenNdx_next = list_indices[i + 1][0]
-            var wModNdx_next = list_indices[i + 1][1]
-            var wInstGenNdx_next = list_indices[i + 1][2]
-            var wInstModNdx_next = list_indices[i + 1][3]
+            var wGenNdx_next = preset_list_indices[i + 1].first
+            var wModNdx_next = preset_list_indices[i + 1].second
 
-            var preset_generators: MutableList<Triple<Int, Int, Int>> = mutableListOf()
+            var preset_generators: MutableList<Generator> = mutableListOf()
             for (j in wGenNdx until wGenNdx_next) {
                 var generator = pgenerators[j]
                 preset_generators.add(generator)
-                if (generator.first == 41) {
+                if (generator.sfGenOper == 41) {
                     break
                 }
             }
 
-            var instrument_generators: MutableList<Triple<Int, Int, Int>> = mutableListOf()
+            var instrument_generators: MutableList<Generator> = mutableListOf()
             for (j in wGenNdx until wGenNdx_next) {
                 var generator = igenerators[j]
                 instrument_generators.add(generator)
-                if (generator.first == 41) {
+                if (generator.sfGenOper == 41) {
                     break
                 }
             }
@@ -238,7 +321,6 @@ class SoundFont {
                 }
             }
 
-
             this.presets.add(
                 Preset(
                     name,
@@ -246,14 +328,59 @@ class SoundFont {
                     pdta_map["phdr"]!!.bytes[(i * 38) + 22] + (pdta_map["phdr"]!!.bytes[(i * 38) + 22] * 256),
                     preset_generators,
                     pmodulators[wModNdx],
-                    instrument_generators,
-                    imodulators[wInstModNdx]
                 )
             )
         }
-
         // Populate Samples
+    }
 
+    private fun modulate(modulatable: ModulatedGenerated, modulator: Modulator) {
+
+    }
+
+    private fun generate_instrument(instrument: Instrument, generators: List<Generator>) {
+        var working_sample = InstrumentSample()
+        for (generator in generators) {
+            when (generator.sfGenOper) {
+                0x35 -> {
+                    instrument.add_sample(working_sample)
+                    working_sample = InstrumentSample()
+                }
+                0x00 -> {
+                    working_sample.sampleStartOffset = if (working_sample.sampleStartOffset == null) {
+                        generator.asInt()
+                    } else {
+                        working_sample.sampleStartOffset!! + generator.asInt()
+                    }
+                }
+                0x01 -> {
+                    working_sample.sampleEndOffset = generator.asInt()
+                }
+                0x02 -> {
+                    working_sample.loopStartOffset = generator.asInt()
+                }
+                0x03 -> {
+                    working_sample.loopEndOffset = generator.asInt()
+                }
+                0x04 -> {
+                    working_sample.sampleStartOffset = if (working_sample.sampleStartOffset == null) {
+                        generator.asInt() * 32768
+                    } else {
+                        working_sample.sampleStartOffset!! + (generator.asInt() * 32768)
+                    }
+                }
+                0x05 -> {
+                    working_sample.mod_lfo_pitch = generator.asInt()
+                }
+                0x06 -> {
+                    working_sample.vib_lfo_pitch = generator.asInt()
+                }
+                0x07 -> {
+                    working_sample.mod_env_pitch = generator.asInt()
+                }
+            }
+
+        }
     }
 }
 
@@ -265,21 +392,39 @@ data class Modulator(
     var sfModTransOper: Int
 )
 
+class Generator(
+    var sfGenOper: Int,
+    var shAmount: Int,
+    var wAmount: Int
+) {
+    fun asInt(): Int {
+        return shAmount + (wAmount * 256)
+    }
+    fun asDouble(): Double {
+
+    }
+}
+
 data class Sample(
     var name: String,
-    var chunk: ByteArray,
+    var start: Int,
+    var end: Int,
     var loopStart: Int,
     var loopEnd: Int,
     var sampleRate: Int,
     var originalPitch: Int,
-    var pithCorrection: Int
+    var pithCorrection: Int,
+    var linkIndex: Int,
+    var sampleType: Int
 )
 
 enum class SFModulator {}
 enum class SFGenerator {}
 enum class Transform {}
 
-abstract class ModulatedGenerated() {
+open class ModulatedGenerated() {
+    //var generators: List<Generator>,
+    //var modulators: List<Modulator>,
     var key_range: Pair<Int, Int>? = null
     var velocity_range: Pair<Int, Int>? = null
     var attenuation: Double? = null
@@ -297,7 +442,6 @@ abstract class ModulatedGenerated() {
     var vol_env_release: Double? = null
     var key_vol_env_hold: Int? = null
     var key_vol_env_decay: Int? = null
-    var mod_env_delay: Double? = null
     var mod_env_attack: Double? = null
     var mod_env_hold: Double? = null
     var mod_env_delay: Double? = null
@@ -307,21 +451,16 @@ abstract class ModulatedGenerated() {
     var mod_env_filter: Int? = null
     var key_mod_env_hold: Int? = null
     var key_mod_env_decay: Int? = null
-    var mod_lfo_delay
-    var mod_lfo_freq
-    var mod_lfo_pitch
-    var mod_lfo_filter
-    var mod_lfo_volume
-    var vib_lfo_delay
-    var vib_lfo_freq
-    var vib_lfo_pitch
-    var chorus
-    var reverb
-
-    fun modulate(sfModSrcOper: Int, sfModDestOper: Int, modAmount: Int, sfModAmtSrcOper: Int, sfModTransOper: Int): {
-    }
-    fun generate(sfGenOper: String, shAmount: Int, wAmount: Int): {
-    }
+    var mod_lfo_delay: Int? = null
+    var mod_lfo_freq: Int? = null
+    var mod_lfo_pitch: Int? = null
+    var mod_lfo_filter: Int? = null
+    var mod_lfo_volume: Int? = null
+    var vib_lfo_delay: Int? = null
+    var vib_lfo_freq: Int? = null
+    var vib_lfo_pitch: Int? = null
+    var chorus: Int? = null
+    var reverb: Int? = null
 }
 
 class Preset(
@@ -329,15 +468,29 @@ class Preset(
     var preset: Int = 0, // MIDI Preset Number
     var bank: Int = 0, // MIDI Bank Number
     // dwLibrary, dwGenre, dwMorphology don't do anything yet
-    var preset_generators: List<Triple<Int, Int, Int>>,
-    var preset_modulator: Modulator,
 ): ModulatedGenerated() {
 
 }
 
-class Instrument(var name: String): ModulatedGenerated() {
-
+class Instrument(var name: String) {
+    var samples: MutableList<InstrumentSample> = mutableListOf()
+    fun add_sample(isample: InstrumentSample) {
+        this.samples.add(isample)
+    }
 }
+class InstrumentSample: ModulatedGenerated() {
+    var sampleIndex: Int = 0
+    var fixedKey: Int? = null
+    var fixedVelocity: Int? = null
+    var sampleStartOffset: Int? = null
+    var sampleEndOffset: Int? = null
+    var loopStartOffset: Int? = null
+    var loopEndOffset: Int? = null
+    var vib_lfo_delay: Double? = null
+    var vib_lf_freq: Double? = null
+    var vib_lfo_pitch: Int? = null
+}
+
 
 
 open class RiffChunk(var type: String)

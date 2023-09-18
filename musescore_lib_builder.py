529402f9788bbf303b1a91c58879bd2c2eeeeac9
Quintin Smith <smith.quintin@protonmail.com>
2023-09-17 23:58:42 -0700

    work on musescore script. still not working

--- musescore_lib_builder.py
+++ musescore_lib_builder.py
@@ -3,6 +3,12 @@ import os
 tree = ET.parse('app/score.mscx')
 root = tree.getroot()
 
+LIST_ELEMENTS = [
+    ( "Score", "Staff", "Measure", "Voice" ),
+]
+
+global_mapped = set()
+
 def camel_to_snake(string):
     for i in range(26):
         string = string.replace(chr(65 + i), f" {chr(97 + i)}")
@@ -56,15 +62,31 @@ def generate_kotlin(tag, path):
     indent_b = " " * (4 * len(path))
     output = f"{indent_a}@Root(strict = false, name = \"{tag.tag}\")\n"
     output += f"{indent_a}class {class_name} {{\n"
-    done = set()
-    for child in tag:
-        if child.tag in done:
-            continue
 
-        done.add(child.tag)
+    new_tag_map = {}
+    for child in tag:
+        if child.tag not in new_tag_map:
+            new_tag_map[child.tag] = ET.Element(child.tag)
+
+        working_child = new_tag_map[child.tag]
+        this_done = set()
+        for grandchild in child:
+            if grandchild.tag not in global_mapped:
+                working_child.append(grandchild)
+            key = path.copy()
+            this_done.add(grandchild.ta)
+        global_mapped = global_mapped.union(this_done)
+        new_tag_map[child.tag] = working_child
+
+        for k, v in child.attrib.items():
+            working_child.attrib[k] = v
+
+    for _, child in new_tag_map.items():
         new_path = path.copy()
         new_path.append(any_to_camel(child.tag))
         output += generate_kotlin(child, new_path) + "\n"
+
+
 
     for k, v in tag.attrib.items():
         attr_name = k.lower().replace(" ", "_")
@@ -75,24 +97,37 @@ def generate_kotlin(tag, path):
         except:
             output += f"{indent_b}var {attr_name}: String? = null"
         output += "\n\n"
+    if tuple(path[1:]) not in LIST_ELEMENTS:
+        output += f"{indent_b}//{path}\n"
+        for name in singles:
+            elm_name = camel_to_snake(name)
+            child_class_name = any_to_camel(name)
+            if child_class_name == "Text":
+                child_class_name = "MSText"
+                elm_name = "mstext"
+            output += f"{indent_b}@field:Element(name = \"{name}\", required = false)\n"
+            output += f"{indent_b}lateinit var {elm_name}: {child_class_name}\n\n"
 
-    for name in singles:
-        elm_name = camel_to_snake(name)
-        child_class_name = any_to_camel(name)
-        if child_class_name == "Text":
-            child_class_name = "MSText"
-            elm_name = "mstext"
-        output += f"{indent_b}@field:Element(name = \"{name}\", required = false)\n"
-        output += f"{indent_b}lateinit var {elm_name}: {child_class_name}\n\n"
-
-    for name in lists:
-        elm_name = camel_to_snake(name)
-        child_class_name = any_to_camel(name)
-        if child_class_name == "Text":
-            child_class_name = "MSText"
-            elm_name = "mstext"
-        output += f"{indent_b}@field:ElementList(entry = \"{name}\", required = false, inline=true)\n"
-        output += f"{indent_b}lateinit var {elm_name}_list: List<{child_class_name}>\n\n"
+        for name in lists:
+            elm_name = camel_to_snake(name)
+            child_class_name = any_to_camel(name)
+            if child_class_name == "Text":
+                child_class_name = "MSText"
+                elm_name = "mstext"
+            output += f"{indent_b}@field:ElementList(entry = \"{name}\", required = false, inline=true)\n"
+            output += f"{indent_b}lateinit var {elm_name}_list: List<{child_class_name}>\n\n"
+    else:
+        indent_c = " " * (4 * (len(path) + 1))
+        output += f"{indent_b}@field:ElementListUnion(\n"
+        lines = []
+        for name in lists:
+            child_class_name = any_to_camel(name)
+            lines.append(f"{indent_c}ElementList(entry = \"{name}\", inline = true, type = {child_class_name}::class, required = false)")
+        for name in singles:
+            child_class_name = any_to_camel(name)
+            lines.append(f"{indent_c}ElementList(entry = \"{name}\", inline = true, type = {child_class_name}::class, required = false)")
+        output += ",\n".join(lines) + f"\n{indent_b})\n"
+        output += f"{indent_b}lateinit var elements: List<Any>\n"
 
     if len(tag) == 0:
         output += f"{indent_b}@field:Text(required = false)\n"
@@ -107,5 +142,6 @@ print("import org.simpleframework.xml.Attribute")
 print("import org.simpleframework.xml.Text")
 print("import org.simpleframework.xml.Element")
 print("import org.simpleframework.xml.ElementList")
+print("import org.simpleframework.xml.ElementListUnion")
 print("import org.simpleframework.xml.Root")
 print(generate_kotlin(root, ["musescore"]))

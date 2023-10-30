a42ef23604e66da3a74d0047f1bb3f90684c268f
Quintin Smith <smith.quintin@protonmail.com>
2023-10-30 11:46:24 -0700

    export_wav working

--- app/src/main/java/com/qfs/apres/soundfontplayer/CachedMidiAudioPlayer.kt
+++ app/src/main/java/com/qfs/apres/soundfontplayer/CachedMidiAudioPlayer.kt
@@ -8,6 +8,7 @@ import com.qfs.apres.soundfont.SoundFont
 import java.io.BufferedOutputStream
 import java.io.DataOutputStream
 import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.OutputStream
 
@@ -47,66 +48,80 @@ open class CachedMidiAudioPlayer(sample_rate: Int, sound_font: SoundFont): MidiP
         var original_delay = this.buffer_delay
         this.buffer_delay = 1
         this.parse_midi(midi)
-        //var tmp_file = File("$path.tmp")
-        var file = File(path)
-        if (file.exists()){
-            file.delete()
-        }
-        var output_stream: OutputStream = FileOutputStream(file)
-        var buffered_output_stream = BufferedOutputStream(output_stream)
 
+        var tmp_file = File("$path.tmp")
+        if (tmp_file.exists()) {
+            tmp_file.delete()
+        }
+
+        var output_stream: OutputStream = FileOutputStream(tmp_file)
+        var buffered_output_stream = BufferedOutputStream(output_stream)
         var data_output_stream = DataOutputStream(buffered_output_stream)
 
-        // 00
-        data_output_stream.writeBytes("RIFF")
-        // 04
-        data_output_stream.writeInt(Integer.reverseBytes(4 + 24 + 8 + (this.frame_count * 2)))
-        // 08
-        data_output_stream.writeBytes("WAVE")
-
-        // 12
-        data_output_stream.writeBytes("fmt ")
-        // 16
-        data_output_stream.writeInt(Integer.reverseBytes(16))
-        // 20
-        data_output_stream.writeShort(0x0100)
-        // 22
-        data_output_stream.writeShort(0x0200)
-        // 24
-        data_output_stream.writeInt(Integer.reverseBytes(this.sample_rate))
-        // 28
-        data_output_stream.writeInt(Integer.reverseBytes(this.sample_rate * 4))
-        // 32
-        data_output_stream.writeByte(0x04)
-        data_output_stream.writeByte(0x00)
-        // 34
-        data_output_stream.writeByte(0x01)
-        data_output_stream.writeByte(0x00)
-        // 36
-        data_output_stream.writeBytes("data")
-        // 40
-        data_output_stream.writeInt(
-            Integer.reverseBytes(this.frame_count * 2)
-        )
-
-        var frame_count = this.frame_count
-        while (frame_count > 0) {
+        var data_byte_count = 0
+        while (true) {
             try {
                 for (b in this.wave_generator.generate().first) {
                     data_output_stream.writeByte((b.toInt() and 0xFF))
                     data_output_stream.writeByte((b.toInt() shr 8))
-                    frame_count -= 1
+                    data_byte_count += 2
                 }
             } catch (e: Exception) {
                 break
             }
         }
 
-        Log.d("AAA", "DONE !${this.frame_count} - $frame_count")
-
-        data_output_stream.flush()
         data_output_stream.close()
 
+
+        var file = File(path)
+        if (file.exists()) {
+            file.delete()
+        }
+
+        output_stream = FileOutputStream(file)
+        buffered_output_stream = BufferedOutputStream(output_stream)
+        data_output_stream = DataOutputStream(buffered_output_stream)
+
+        // 00 Riff
+        data_output_stream.writeBytes("RIFF")
+        // 04 File size
+        data_output_stream.writeInt(Integer.reverseBytes(4 + 24 + 8 + data_byte_count))
+        // 08 WAVE
+        data_output_stream.writeBytes("WAVE")
+
+        // 12 'fmt '
+        data_output_stream.writeBytes("fmt ")
+        // 16 chunk size (always 16)
+        data_output_stream.writeInt(Integer.reverseBytes(16))
+        // 20 (WAVE_FORMAT_PCM code == 1)
+        data_output_stream.writeShort(0x0100)
+        // 22 Channel Count
+        data_output_stream.writeShort(0x0200)
+        // 24 Sample rate
+        data_output_stream.writeInt(Integer.reverseBytes(this.sample_rate))
+        // 28 byte rate
+        data_output_stream.writeInt(Integer.reverseBytes(this.sample_rate * 2))
+        // 32 Block Alignment
+        data_output_stream.writeByte(0x04)
+        data_output_stream.writeByte(0x00)
+        // 34 Bits per sample
+        data_output_stream.writeByte(0x10)
+        data_output_stream.writeByte(0x00)
+        // 36 "data"
+        data_output_stream.writeBytes("data")
+        // 40 Chunk size
+        data_output_stream.writeInt( Integer.reverseBytes(data_byte_count) )
+
+        val input_stream = tmp_file.inputStream()
+        input_stream.copyTo(
+            data_output_stream
+        )
+
+        input_stream.close()
+        data_output_stream.close()
+
+        tmp_file.delete()
 
         this.buffer_delay = original_delay
     }

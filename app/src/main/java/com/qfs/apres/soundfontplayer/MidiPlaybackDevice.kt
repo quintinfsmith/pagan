b940fea4607f43aed3bd8c8a377ddef13187631b
Quintin Smith <smith.quintin@protonmail.com>
2023-11-07 23:37:44 -0800

    fill chunks to buffer delay instead of limit to unpause write

--- app/src/main/java/com/qfs/apres/soundfontplayer/MidiPlaybackDevice.kt
+++ app/src/main/java/com/qfs/apres/soundfontplayer/MidiPlaybackDevice.kt
@@ -77,7 +77,6 @@ open class MidiPlaybackDevice(
 
             var flag_no_more_chunks = false
             var pause_write = true
-            var first_pass = true
 
             thread {
                 while (!flag_no_more_chunks && this@MidiPlaybackDevice.stop_request != StopRequest.Kill) {
@@ -89,10 +88,9 @@ open class MidiPlaybackDevice(
                                         this@MidiPlaybackDevice.wave_generator.generate(this@MidiPlaybackDevice.sample_handle_manager.buffer_size)
                                     chunks.add(chunk)
                                 }
-                                if (first_pass) {
+                                if (pause_write && chunks.size >= this@MidiPlaybackDevice.buffer_delay) {
                                     pause_write = false
                                 }
-                                first_pass = false
                             }
                         } catch (e: WaveGenerator.KilledException) {
                             flag_no_more_chunks = true

b28eebb891adfe21922ea3a1b7cfafe3a0ab14b0
Quintin Smith <smith.quintin@protonmail.com>
2024-03-11 12:56:27 -0700

    improved some variable names

--- app/src/main/java/com/qfs/apres/soundfontplayer/SampleHandleGenerator.kt
+++ app/src/main/java/com/qfs/apres/soundfontplayer/SampleHandleGenerator.kt
@@ -45,23 +45,23 @@ class SampleHandleGenerator(var sample_rate: Int, var buffer_size: Int) {
         return map_key
     }
 
-    fun generate_new(note: Int, bend: Int, sample: SampleDirective, global_sample_directive: SampleDirective, instrument: InstrumentDirective, global_instrument_directive: InstrumentDirective, linked_handle_count: Int = 1): SampleHandle {
+    fun generate_new(note: Int, bend: Int, sample_directive: SampleDirective, global_sample_directive: SampleDirective, instrument_directive: InstrumentDirective, global_instrument_directive: InstrumentDirective, linked_handle_count: Int = 1): SampleHandle {
         var pitch_shift = 1F
 
-        val original_note = sample.root_key ?: sample.sample!!.originalPitch
+        val original_note = sample_directive.root_key ?: sample_directive.sample!!.originalPitch
 
         // TODO: Why did I do this check? I vaguely remember needing it but I need a note
         val target_pitch = if (original_note != 255) {
-            val tuning_cent: Int = (sample.tuning_cent ?: global_sample_directive.tuning_cent ?: 0 ) + (instrument.tuning_cent ?: 0) + (global_instrument_directive.tuning_cent ?: 0)
-            var tuning_semi: Float = ((sample.tuning_semi ?: global_sample_directive.tuning_semi ?: 0 )
-                + (instrument.tuning_semi ?: 0)
+            val tuning_cent: Int = (sample_directive.tuning_cent ?: global_sample_directive.tuning_cent ?: 0 ) + (instrument_directive.tuning_cent ?: 0) + (global_instrument_directive.tuning_cent ?: 0)
+            var tuning_semi: Float = ((sample_directive.tuning_semi ?: global_sample_directive.tuning_semi ?: 0 )
+                + (instrument_directive.tuning_semi ?: 0)
                 + (global_instrument_directive.tuning_semi ?: 0)).toFloat()
 
             // Skip tuning if we can
             if (tuning_cent == 0 && tuning_semi == 0F && note == original_note && bend == 0) {
                 1F
             } else {
-                tuning_semi += tuning_cent / 100F
+                tuning_semi += tuning_cent.toFloat() / 100F
                 val original_pitch = (2F).pow(original_note.toFloat() / 12F)
                 val required_pitch = (2F).pow((note.toFloat() + tuning_semi + (bend.toFloat() / 512F)) / 12F)
                 pitch_shift = required_pitch / original_pitch
@@ -71,72 +71,72 @@ class SampleHandleGenerator(var sample_rate: Int, var buffer_size: Int) {
             1F
         }
 
-        if (sample.sample!!.sampleRate != this.sample_rate) {
-            pitch_shift *= (sample.sample!!.sampleRate.toFloat() / this.sample_rate.toFloat())
+        if (sample_directive.sample!!.sampleRate != this.sample_rate) {
+            pitch_shift *= (sample_directive.sample!!.sampleRate.toFloat() / this.sample_rate.toFloat())
         }
 
-        val data = sample.sample!!.data!!
-        val initial_attenuation: Float = (sample.attenuation ?: global_sample_directive.attenuation ?: 0F) + (instrument.attenuation ?: 0F) + (global_instrument_directive.attenuation ?: 0F)
-        val vol_env_sustain: Float = (sample.vol_env_sustain ?: global_sample_directive.vol_env_sustain ?: 0F) + (instrument.vol_env_sustain ?: 0F) + (global_instrument_directive.vol_env_sustain ?: 0F)
+        val data = sample_directive.sample!!.data!!
+        val initial_attenuation: Float = (sample_directive.attenuation ?: global_sample_directive.attenuation ?: 0F) + (instrument_directive.attenuation ?: 0F) + (global_instrument_directive.attenuation ?: 0F)
+        val vol_env_sustain: Float = (sample_directive.vol_env_sustain ?: global_sample_directive.vol_env_sustain ?: 0F) + (instrument_directive.vol_env_sustain ?: 0F) + (global_instrument_directive.vol_env_sustain ?: 0F)
         val volume_envelope = SampleHandle.VolumeEnvelope(
             sample_rate = this.sample_rate,
-            delay = (sample.vol_env_delay ?: global_sample_directive.vol_env_delay ?: 0F )
-                * (instrument.vol_env_delay ?: 1F)
+            delay = (sample_directive.vol_env_delay ?: global_sample_directive.vol_env_delay ?: 0F )
+                * (instrument_directive.vol_env_delay ?: 1F)
                 * (global_instrument_directive.vol_env_delay ?: 1F),
-            attack = (sample.vol_env_attack ?: global_sample_directive.vol_env_attack ?: 0F )
-                * (instrument.vol_env_attack ?: 1F)
+            attack = (sample_directive.vol_env_attack ?: global_sample_directive.vol_env_attack ?: 0F )
+                * (instrument_directive.vol_env_attack ?: 1F)
                 * (global_instrument_directive.vol_env_attack ?: 1F),
-            hold = (sample.vol_env_hold ?: global_sample_directive.vol_env_hold ?: 0F )
-                * (instrument.vol_env_hold ?: 1F)
+            hold = (sample_directive.vol_env_hold ?: global_sample_directive.vol_env_hold ?: 0F )
+                * (instrument_directive.vol_env_hold ?: 1F)
                 * (global_instrument_directive.vol_env_hold ?: 1F),
-            decay = (sample.vol_env_decay ?: global_sample_directive.vol_env_decay ?: 0F )
-                * (instrument.vol_env_decay ?: 1F)
+            decay = (sample_directive.vol_env_decay ?: global_sample_directive.vol_env_decay ?: 0F )
+                * (instrument_directive.vol_env_decay ?: 1F)
                 * (global_instrument_directive.vol_env_decay ?: 1F),
-            release = (sample.vol_env_release ?: global_sample_directive.vol_env_release ?: 0F )
-                * (instrument.vol_env_release ?: 1F)
+            release = (sample_directive.vol_env_release ?: global_sample_directive.vol_env_release ?: 0F )
+                * (instrument_directive.vol_env_release ?: 1F)
                 * (global_instrument_directive.vol_env_release ?: 1F),
             sustain_attenuation = 1F - (max(0F, min(vol_env_sustain, 1000F)) / 100F)
         )
 
-        val mod_env_sustain = (sample.mod_env_sustain ?: global_sample_directive.mod_env_sustain ?: 0F) * (instrument.mod_env_sustain ?: 0F) * (global_instrument_directive.mod_env_sustain ?: 0F)
+        val mod_env_sustain = (sample_directive.mod_env_sustain ?: global_sample_directive.mod_env_sustain ?: 0F) * (instrument_directive.mod_env_sustain ?: 0F) * (global_instrument_directive.mod_env_sustain ?: 0F)
         val modulation_envelope = SampleHandle.ModulationEnvelope(
             sample_rate = this.sample_rate,
-            delay = (sample.mod_env_delay ?: global_sample_directive.mod_env_delay ?: 0F )
-                * (instrument.mod_env_delay ?: 1F)
+            delay = (sample_directive.mod_env_delay ?: global_sample_directive.mod_env_delay ?: 0F )
+                * (instrument_directive.mod_env_delay ?: 1F)
                 * (global_instrument_directive.mod_env_delay ?: 1F),
-            attack = (sample.mod_env_attack ?: global_sample_directive.mod_env_attack ?: 0F )
-                * (instrument.mod_env_attack ?: 1F)
+            attack = (sample_directive.mod_env_attack ?: global_sample_directive.mod_env_attack ?: 0F )
+                * (instrument_directive.mod_env_attack ?: 1F)
                 * (global_instrument_directive.mod_env_attack ?: 1F),
-            hold = (sample.mod_env_hold ?: global_sample_directive.mod_env_hold ?: 0F)
-                * (instrument.mod_env_hold ?: 1F)
+            hold = (sample_directive.mod_env_hold ?: global_sample_directive.mod_env_hold ?: 0F)
+                * (instrument_directive.mod_env_hold ?: 1F)
                 * (global_instrument_directive.mod_env_hold ?: 1F),
-            decay = (sample.mod_env_decay ?: global_sample_directive.mod_env_decay ?: 0F)
-                * (instrument.mod_env_decay ?: 1F)
+            decay = (sample_directive.mod_env_decay ?: global_sample_directive.mod_env_decay ?: 0F)
+                * (instrument_directive.mod_env_decay ?: 1F)
                 * (global_instrument_directive.mod_env_decay ?: 1F),
-            release = (sample.mod_env_release ?: global_sample_directive.mod_env_release ?: 0F)
-                * (instrument.mod_env_release ?: 1F)
+            release = (sample_directive.mod_env_release ?: global_sample_directive.mod_env_release ?: 0F)
+                * (instrument_directive.mod_env_release ?: 1F)
                 * (global_instrument_directive.mod_env_release ?: 1F),
             sustain_attenuation = 1F - (max(0F, min(mod_env_sustain, 1000F)) / 100F)
         )
 
-        val mod_lfo_freq: Float = (sample.mod_lfo_freq ?: global_sample_directive.mod_lfo_freq ?: 0F) * (instrument.mod_lfo_freq ?: 1F) * (global_instrument_directive.mod_lfo_freq ?: 1F)
-        val mod_lfo_delay: Float = (sample.mod_lfo_delay ?: global_sample_directive.mod_lfo_delay ?: 0F) * (instrument.mod_lfo_delay ?: 1F) * (global_instrument_directive.mod_lfo_delay ?: 1F)
-        val mod_lfo_to_volume: Float = (sample.mod_lfo_to_volume ?: global_sample_directive.mod_lfo_to_volume ?: 0F ) + (instrument.mod_lfo_to_volume ?: 0F) + (global_instrument_directive.mod_lfo_to_volume ?: 0F)
-        val mod_lfo_pitch: Int = (sample.mod_lfo_pitch ?: global_sample_directive.mod_lfo_pitch ?: 0 ) + (instrument.mod_lfo_pitch ?: 0) + (global_instrument_directive.mod_lfo_pitch ?: 0)
-        val mod_lfo_filter: Int = (sample.mod_lfo_filter ?: global_sample_directive.mod_lfo_filter ?: 0 ) + (instrument.mod_lfo_filter ?: 0) + (global_instrument_directive.mod_lfo_filter ?: 0)
-        val filter_cutoff: Float = (sample.filter_cutoff ?: global_sample_directive.filter_cutoff ?: 13500F ) + (instrument.filter_cutoff ?: 0F) + (global_instrument_directive.filter_cutoff ?: 0F)
+        val mod_lfo_freq: Float = (sample_directive.mod_lfo_freq ?: global_sample_directive.mod_lfo_freq ?: 0F) * (instrument_directive.mod_lfo_freq ?: 1F) * (global_instrument_directive.mod_lfo_freq ?: 1F)
+        val mod_lfo_delay: Float = (sample_directive.mod_lfo_delay ?: global_sample_directive.mod_lfo_delay ?: 0F) * (instrument_directive.mod_lfo_delay ?: 1F) * (global_instrument_directive.mod_lfo_delay ?: 1F)
+        val mod_lfo_to_volume: Float = (sample_directive.mod_lfo_to_volume ?: global_sample_directive.mod_lfo_to_volume ?: 0F ) + (instrument_directive.mod_lfo_to_volume ?: 0F) + (global_instrument_directive.mod_lfo_to_volume ?: 0F)
+        val mod_lfo_pitch: Int = (sample_directive.mod_lfo_pitch ?: global_sample_directive.mod_lfo_pitch ?: 0 ) + (instrument_directive.mod_lfo_pitch ?: 0) + (global_instrument_directive.mod_lfo_pitch ?: 0)
+        val mod_lfo_filter: Int = (sample_directive.mod_lfo_filter ?: global_sample_directive.mod_lfo_filter ?: 0 ) + (instrument_directive.mod_lfo_filter ?: 0) + (global_instrument_directive.mod_lfo_filter ?: 0)
+        val filter_cutoff: Float = (sample_directive.filter_cutoff ?: global_sample_directive.filter_cutoff ?: 13500F ) + (instrument_directive.filter_cutoff ?: 0F) + (global_instrument_directive.filter_cutoff ?: 0F)
         this.generated += 1
 
         return SampleHandle(
             data = data,
             sample_rate = sample_rate,
-            pan = (sample.pan ?: instrument.pan ?: global_instrument_directive.pan ?: 0F) * 100F / 500F,
+            pan = (sample_directive.pan ?: instrument_directive.pan ?: global_instrument_directive.pan ?: 0F) * 100F / 500F,
             pitch_shift = pitch_shift,
             initial_attenuation = 1F - (initial_attenuation / 100F),
-            stereo_mode = sample.sample!!.sampleType,
-            loop_points = if (sample.sampleMode != null && sample.sampleMode!! and 1 == 1) {
-                val start = sample.sample!!.loopStart + (sample.loopStartOffset ?: 0) + (global_sample_directive.loopStartOffset ?: 0)
-                val end = sample.sample!!.loopEnd + (sample.loopEndOffset ?: 0) + (global_sample_directive.loopEndOffset ?: 0)
+            stereo_mode = sample_directive.sample!!.sampleType,
+            loop_points = if (sample_directive.sampleMode != null && sample_directive.sampleMode!! and 1 == 1) {
+                val start = sample_directive.sample!!.loopStart + (sample_directive.loopStartOffset ?: 0) + (global_sample_directive.loopStartOffset ?: 0)
+                val end = sample_directive.sample!!.loopEnd + (sample_directive.loopEndOffset ?: 0) + (global_sample_directive.loopEndOffset ?: 0)
                 val size = (end - start).toFloat() / pitch_shift
                 val pitched_start = start.toFloat() / pitch_shift
                 Pair(pitched_start.toInt(), (pitched_start + size).toInt())

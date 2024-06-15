06ab17930090522c7066b20db167ef94176fba28
Quintin Smith <smith.quintin@protonmail.com>
2024-06-15 12:03:24 -0700

    renamed unit test files

--- app/src/test/java/com/qfs/pagan/OpusLayerBaseUnitTest.kt
+++ app/src/test/java/com/qfs/pagan/OpusLayerBaseUnitTest.kt
@@ -27,7 +27,7 @@ import com.qfs.apres.event.BankSelect
 import com.qfs.apres.event.ProgramChange
 
 
-import com.qfs.pagan.opusmanager.OpusLayerLinks as OpusManager
+import com.qfs.pagan.opusmanager.OpusLayerBase as OpusManager
 
 class OpusLayerBaseUnitTest {
     @Test

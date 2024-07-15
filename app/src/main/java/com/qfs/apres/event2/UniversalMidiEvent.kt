8564880442198c38fbf74c9953c4883267879556
Quintin Smith <smith.quintin@protonmail.com>
2024-07-15 16:16:38 -0700

    implemented a few Midi UMP messages

--- app/src/main/java/com/qfs/apres/event2/UniversalMidiEvent.kt
+++ app/src/main/java/com/qfs/apres/event2/UniversalMidiEvent.kt
@@ -4,6 +4,89 @@ import com.qfs.apres.event.GeneralMIDIEvent
 
 abstract interface UMPEvent: GeneralMIDIEvent
 
+class PolyPressure(
+    var channel: Int,
+    var index: Int,
+    var pressure: Float,
+    var group: Int = 0
+): UMPEvent {
+    override fun as_bytes(): ByteArray {
+        val pressure_int = (this.pressure * 0xFFFFFFFF).toInt()
+        return byteArrayOf(
+            (0x40 or (this.group and 0x0F)).toByte(),
+            (0xA0 or (this.channel and 0x0F)).toByte(),
+            (this.index and 0x7F).toByte(),
+            0x0.toByte(),
+
+            // Assuming little endian
+            ((pressure_int shr 24) and 0xFF).toByte(),
+            ((pressure_int shr 16) and 0xFF).toByte(),
+            ((pressure_int shr 8) and 0xFF).toByte(),
+            (pressure_int and 0xFF).toByte(),
+        )
+    }
+}
+class ChannelPressure(
+    var channel: Int,
+    var pressure: Float,
+    var group: Int = 0
+): UMPEvent {
+    override fun as_bytes(): ByteArray {
+        val pressure_int = (this.pressure * 0xFFFFFFFF).toInt()
+        return byteArrayOf(
+            (0x40 or (this.group and 0x0F)).toByte(),
+            (0xD0 or (this.channel and 0x0F)).toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            // Assuming little endian
+            ((pressure_int shr 24) and 0xFF).toByte(),
+            ((pressure_int shr 16) and 0xFF).toByte(),
+            ((pressure_int shr 8) and 0xFF).toByte(),
+            (pressure_int and 0xFF).toByte(),
+        )
+    }
+}
+
+class ChannelPitchBend(
+    var channel: Int,
+    var bend: Float,
+    var group: Int = 0
+): UMPEvent {
+    override fun as_bytes(): ByteArray {
+        val bend_int = (((this.bend + 1F) / 2F) * 0xFFFFFFFF).toInt()
+        return byteArrayOf(
+            (0x40 or (this.group and 0x0F)).toByte(),
+            (0xE0 or (this.channel and 0x0F)).toByte(),
+            0x00.toByte(),
+            ((bend_int shr 24) and 0xFF).toByte(),
+            ((bend_int shr 16) and 0xFF).toByte(),
+            ((bend_int shr 8) and 0xFF).toByte(),
+            (bend_int and 0xFF).toByte()
+        )
+    }
+}
+
+class PerNotePitchBend(
+    val index: Int = 0,
+    var channel: Int,
+    var bend: Float,
+    var group: Int = 0
+): UMPEvent {
+    override fun as_bytes(): ByteArray {
+        val bend_int = (((this.bend + 1F) / 2F) * 0xFFFFFFFF).toInt()
+        return byteArrayOf(
+            (0x40 or (this.group and 0x0F)).toByte(),
+            (0x60 or (this.channel and 0x0F)).toByte(),
+            (this.index and 0x7F).toByte(),
+            0x00.toByte(),
+            ((bend_int shr 24) and 0xFF).toByte(),
+            ((bend_int shr 16) and 0xFF).toByte(),
+            ((bend_int shr 8) and 0xFF).toByte(),
+            (bend_int and 0xFF).toByte()
+        )
+    }
+}
+
 class NoteOn79(
     var index: Int = 0,
     var channel: Int,
@@ -45,3 +128,53 @@ class NoteOff79(
         )
     }
 }
+
+class ProgramChange(
+    var index: Int,
+    var channel: Int,
+    var group: Int,
+    var program: Int,
+    var bank: Int,
+    var bank_select: Boolean = false,
+): UMPEvent {
+    override fun as_bytes(): ByteArray {
+        var option_flags = 0x00
+        if (this.bank_select) {
+            option_flags += 1
+        }
+
+        return byteArrayOf(
+            (0x40 or (this.group and 0x0F)).toByte(),
+            (0xC0 or (this.channel and 0x0F)).toByte(),
+            0x00.toByte(),
+            option_flags.toByte(),
+            (0x7F and this.program).toByte(),
+            0x00.toByte(),
+            ((this.bank and 0x3f80) shr 7).toByte(),
+            (this.bank and 0x007F).toByte()
+        )
+    }
+}
+
+class ControlChange(
+    var index: Int,
+    var group: Int,
+    var status: Int,
+    var channel: Int,
+    var value: Int
+): UMPEvent {
+    override fun as_bytes(): ByteArray {
+        return byteArrayOf(
+            (0x40 or (this.group and 0x0F)).toByte(),
+            ((this.status shl 4) or (this.channel and 0x0F)).toByte(),
+            (this.index and 0xFF).toByte(),
+            0x00.toByte(),
+            ((this.value shr 24) and 0xFF).toByte(),
+            ((this.value shr 16) and 0xFF).toByte(),
+            ((this.value shr 8) and 0xFF).toByte(),
+            (this.value and 0xFF).toByte()
+        )
+    }
+}
+
+

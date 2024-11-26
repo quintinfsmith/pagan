d2af957259f15c32aec09cbf35767c9609f720dd
Quintin Smith <smith.quintin@protonmail.com>
2024-11-25 18:40:01 -0800

    clean up drawing pan slider

--- app/src/main/java/com/qfs/pagan/PanSliderWidget.kt
+++ app/src/main/java/com/qfs/pagan/PanSliderWidget.kt
@@ -31,9 +31,7 @@ class PanSliderWidget(context: Context, attrs: AttributeSet? = null): LinearLayo
     var on_change_listener: OnSeekBarChangeListener? = null
     val image_view: ImageView = object: androidx.appcompat.widget.AppCompatImageView(context, attrs) {
         val paint = Paint()
-        val bar_path = Path()
-        init {
-        }
+        val path = Path()
 
         fun get_main_activity(): MainActivity {
             var context = this@PanSliderWidget.context
@@ -54,55 +52,62 @@ class PanSliderWidget(context: Context, attrs: AttributeSet? = null): LinearLayo
             val offset = width * div_size
             val offset_half = offset / 2f
             val handle_center = relative_n + (.5F / ((that.max + 1) - that.min).toFloat())
-            val purple = color_map.get(ColorMap.Palette.Leaf)
+            val purple = color_map[ColorMap.Palette.Leaf]
 
             this.paint.strokeWidth = 1f
             this.paint.color = purple
             this.paint.setShadowLayer(5F, 2F, 2F, Color.BLACK)
+            val end_clip = if (that.progress == that.min || that.progress == that.max) {
+                (div_size / 2F)
+            } else {
+                0F
+            }
+            val bar_start = max(0F, handle_center - .5F + end_clip) * width
+            val bar_end = min(1F, handle_center + .5f - end_clip) * width
 
-            val main_path = Path()
-            val bar_start = max(0F, (handle_center - .5F)) * width
-            val bar_end = min(1F, handle_center + .5f) * width
-            main_path.moveTo(bar_start, 0f)
-            main_path.lineTo(bar_end, 0F)
-            main_path.lineTo(bar_end, height)
-            main_path.lineTo(bar_start, height)
-            canvas.drawPath(main_path, this.paint)
+            this.path.reset()
+            this.path.moveTo(bar_start, 0f)
+            this.path.lineTo(bar_end, 0F)
+            this.path.lineTo(bar_end, height)
+            this.path.lineTo(bar_start, height)
+
+            canvas.drawPath(this.path, this.paint)
 
             val handle_point = width * relative_n
 
             this.paint.strokeWidth = 1F
             this.paint.color = Color.WHITE
+
             canvas.drawLine(0f, 0F, width, 0F, this.paint)
             canvas.drawLine(0f, height - 1F, width, height - 1F, this.paint)
             canvas.drawLine(0F, 0F, 0F, height, this.paint)
             canvas.drawLine(width - 1F, 0F, width - 1F, height, this.paint)
 
-            this.paint.setColor(color_map.get(ColorMap.Palette.LeafSelected))
+            this.paint.color = color_map[ColorMap.Palette.LeafSelected]
             this.paint.strokeWidth = 3f
 
-            val position_path = Path()
+            this.path.reset()
             when (that.progress) {
                 that.max -> {
-                    position_path.moveTo(handle_point + offset, height * .9F)
-                    position_path.lineTo(handle_point + offset_half, height / 2)
-                    position_path.lineTo(handle_point + offset, height * .1F)
+                    this.path.moveTo(handle_point + offset, height * .9F)
+                    this.path.lineTo(handle_point + offset_half, height / 2)
+                    this.path.lineTo(handle_point + offset, height * .1F)
                 }
                 that.min -> {
-                    position_path.moveTo(handle_point, height * .9F)
-                    position_path.lineTo(handle_point + offset_half, height / 2)
-                    position_path.lineTo(handle_point, height * .1F)
+                    this.path.moveTo(handle_point, height * .9F)
+                    this.path.lineTo(handle_point + offset_half, height / 2)
+                    this.path.lineTo(handle_point, height * .1F)
                 }
                 else -> {
-                    position_path.moveTo(handle_point + offset_half, height * .9F)
-                    position_path.lineTo(handle_point, height / 2F)
-                    position_path.lineTo(handle_point + offset_half, height * .1F)
-                    position_path.lineTo(handle_point + offset, height / 2F)
-                    position_path.lineTo(handle_point + offset_half, height * .9F)
+                    this.path.moveTo(handle_point + offset_half, height * .9F)
+                    this.path.lineTo(handle_point, height / 2F)
+                    this.path.lineTo(handle_point + offset_half, height * .1F)
+                    this.path.lineTo(handle_point + offset, height / 2F)
+                    this.path.lineTo(handle_point + offset_half, height * .9F)
                 }
             }
 
-            canvas.drawPath(position_path, this.paint)
+            canvas.drawPath(this.path, this.paint)
 
             super.onDraw(canvas)
         }

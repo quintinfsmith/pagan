a8204e98367e1fcef41a557f2b4db3975ab8f139
Quintin F Smith <smith.quintin@protonmail.com>
2025-04-15 21:17:25 -0700

    created Generated.cpp

---
+++ app/src/main/cpp/soundfont/Generated.h
@@ -0,0 +1,12 @@
+#include <jni.h>
+#include <iostream>
+#include <string>
+#include <exception>
+
+
+#ifndef PAGAN_GENERATED_H
+#define PAGAN_GENERATED_H
+
+class Generated;
+
+#endif //PAGAN_GENERATED_H

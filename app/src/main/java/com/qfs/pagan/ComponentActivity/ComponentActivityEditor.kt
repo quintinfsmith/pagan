4b2f8db553d331f38ca92e54b4cfe367f27bc17c
Quintin Smith <smith.quintin@protonmail.com>
2025-11-14 14:05:32 -0800

    Working on setting up framework for composable editor

--- app/src/main/java/com/qfs/pagan/ComponentActivity/ComponentActivityEditor.kt
+++ app/src/main/java/com/qfs/pagan/ComponentActivity/ComponentActivityEditor.kt
@@ -1,26 +1,97 @@
 package com.qfs.pagan.ComponentActivity
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.qfs.pagan.composable.cxtmenu.ContextMenuChannelPrimary
+import com.qfs.pagan.composable.cxtmenu.ContextMenuChannelSecondary
+import com.qfs.pagan.composable.cxtmenu.ContextMenuColumnPrimary
+import com.qfs.pagan.composable.cxtmenu.ContextMenuColumnSecondary
+import com.qfs.pagan.composable.cxtmenu.ContextMenuLinePrimary
+import com.qfs.pagan.composable.cxtmenu.ContextMenuLineSecondary
+import com.qfs.pagan.composable.cxtmenu.ContextMenuRangePrimary
+import com.qfs.pagan.composable.cxtmenu.ContextMenuRangeSecondary
+import com.qfs.pagan.composable.cxtmenu.ContextMenuSinglePrimary
+import com.qfs.pagan.composable.cxtmenu.ContextMenuSingleSecondary
+import com.qfs.pagan.structure.opusmanager.base.OpusEvent
+import com.qfs.pagan.structure.opusmanager.cursor.CursorMode
+import com.qfs.pagan.structure.rationaltree.ReducibleTree
+import com.qfs.pagan.uibill.UIChangeBill
+import com.qfs.pagan.viewmodel.ViewModelEditor
 
 class ComponentActivityEditor: PaganComponentActivity() {
-    @Composable
-    fun ContextMenuPrimary() {
-
+    val model_editor: ViewModelEditor by this.viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
     @Composable
-    fun ContextMenuSecondary() {
-
+    fun ContextMenuPrimary(ui_facade: UIChangeBill) {
+        val cursor = ui_facade.active_cursor
+        when (cursor?.type) {
+            CursorMode.Line -> ContextMenuLinePrimary(ui_facade)
+            CursorMode.Column -> ContextMenuColumnPrimary(ui_facade)
+            CursorMode.Single -> ContextMenuSinglePrimary(ui_facade)
+            CursorMode.Range -> ContextMenuRangePrimary(ui_facade)
+            CursorMode.Channel -> ContextMenuChannelPrimary(ui_facade)
+            CursorMode.Unset,
+            null -> TODO()
+        }
+    }
+    @Composable
+    fun ContextMenuSecondary(ui_facade: UIChangeBill) {
+        val cursor = ui_facade.active_cursor
+        when (cursor?.type) {
+            CursorMode.Line -> ContextMenuLineSecondary(ui_facade)
+            CursorMode.Column -> ContextMenuColumnSecondary(ui_facade)
+            CursorMode.Single -> ContextMenuSingleSecondary(ui_facade)
+            CursorMode.Range -> ContextMenuRangeSecondary(ui_facade)
+            CursorMode.Channel -> ContextMenuChannelSecondary(ui_facade)
+            CursorMode.Unset,
+            null -> TODO()
+        }
     }
 
     @Composable
-    fun MainTable() {
+    fun MainTable(ui_facade: UIChangeBill) {
+        Column() {
+            ui_facade.cell_map.forEach { line ->
+                Row() {
+                    line.forEach { cell ->
+                        Column() {
+                            CellView(cell)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun CellView(cell: ReducibleTree<out OpusEvent>) {
+        Row {
+            composable_traverse(cell) { tree, event ->
+                if (tree.is_leaf()) {
+                    Text("C")
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun <T> composable_traverse(tree: ReducibleTree<T>, callback: @Composable (ReducibleTree<T>, T?) -> Unit) {
+        if (! tree.is_leaf()) {
+            for ((_, child) in tree.divisions) {
+                composable_traverse(child, callback)
+            }
+        }
+        callback(tree, tree.event)
     }
 
     @Composable
@@ -35,17 +106,21 @@ class ComponentActivityEditor: PaganComponentActivity() {
 
     @Composable
     override fun LayoutMediumPortrait() {
+        val view_model = this.model_editor
+        val ui_facade = model_editor.opus_manager.ui_facade
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.BottomCenter
         ) {
             Box(Modifier.fillMaxSize()) {
-                MainTable()
+                MainTable(ui_facade)
             }
-            Box(Modifier) {
-                Column {
-                    Row { ContextMenuPrimary() }
-                    Row { ContextMenuSecondary() }
+            if (ui_facade.active_cursor != null && ui_facade.active_cursor!!.type != CursorMode.Unset) {
+                Box(Modifier) {
+                    Column {
+                        Row { ContextMenuPrimary(ui_facade) }
+                        Row { ContextMenuSecondary(ui_facade) }
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/qfs/pagan/composable/cxtmenu/cxtmenu.kt
@@ -0,0 +1,29 @@
+package com.qfs.pagan.composable.cxtmenu
+
+import androidx.compose.runtime.Composable
+import com.qfs.pagan.uibill.UIChangeBill
+
+@Composable
+fun ContextMenuLinePrimary(ui_facade: UIChangeBill) {}
+@Composable
+fun ContextMenuLineSecondary(ui_facade: UIChangeBill) {}
+
+@Composable
+fun ContextMenuColumnPrimary(ui_facade: UIChangeBill) {}
+@Composable
+fun ContextMenuColumnSecondary(ui_facade: UIChangeBill) {}
+
+@Composable
+fun ContextMenuSinglePrimary(ui_facade: UIChangeBill) {}
+@Composable
+fun ContextMenuSingleSecondary(ui_facade: UIChangeBill) {}
+
+@Composable
+fun ContextMenuRangePrimary(ui_facade: UIChangeBill) {}
+@Composable
+fun ContextMenuRangeSecondary(ui_facade: UIChangeBill) {}
+
+@Composable
+fun ContextMenuChannelPrimary(ui_facade: UIChangeBill) {}
+@Composable
+fun ContextMenuChannelSecondary(ui_facade: UIChangeBill) {}

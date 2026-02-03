c52e8d86afe5d716b8493d8637f9dad417ddf05e
Quintin F Smith <smith.quintin@protonmail.com>
2026-02-02 23:04:14 -0800

    attributionTag

--- app/src/main/java/com/qfs/pagan/ComponentActivity/PaganComponentActivity.kt
+++ app/src/main/java/com/qfs/pagan/ComponentActivity/PaganComponentActivity.kt
@@ -2,6 +2,7 @@ package com.qfs.pagan.ComponentActivity
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.DocumentsContract
 import android.provider.DocumentsContract.Document.COLUMN_DOCUMENT_ID

1519d2ce9c556148c61b501478e5eeaea3305791
Quintin F Smith <smith.quintin@protonmail.com>
2026-06-20 10:46:33 -0700

    tidying up Color logic

--- app/src/main/java/com/qfs/pagan/ui/theme/Colors.kt
+++ app/src/main/java/com/qfs/pagan/ui/theme/Colors.kt
@@ -15,33 +15,70 @@ import kotlin.math.min
 
 object Colors {
     val LEAF_COLOR = Color(0xFF765bd5)
+    val EFFECT_COLOR = Color(0xFFCB9C20)
+    val LINE_COLOR = Color(0xFFE0E0E0)
+    val LINE_COLOR_NIGHT = Color(0xFF232323)
+
+    val SPILL: (Color) -> Color = { base_color ->
+        if ((base_color.red + base_color.green + base_color.blue) / 3F > .5F) {
+            Color(
+                red = base_color.red * .75F,
+                green = base_color.green * .75F,
+                blue = base_color.blue * .75F,
+                alpha = base_color.alpha
+            )
+        } else {
+            Color(
+                red = min(1F, base_color.red / .75F),
+                green = min(1F, base_color.green / .75F),
+                blue = min(1F, base_color.blue / .75F),
+                alpha = base_color.alpha
+            )
+        }
+    }
+
+    val LINE_SELECTED = Color(0xFF5BA1D6)
+    val SELECTION = Color(0xFF0033AA)
+
+    val SELECTED_PRIMARY: (Boolean, Color) -> Color = { is_empty, base_color ->
+        val weight = .2F
+        if (is_empty) {
+            Color(
+                red = (base_color.red * weight) + (LINE_SELECTED.red * (1F - weight)),
+                green = (base_color.green * weight) + (LINE_SELECTED.green * (1F - weight)),
+                blue = (base_color.blue * weight) + (LINE_SELECTED.blue * (1F - weight)),
+            )
+        } else {
+            Color(
+                red = (base_color.red * weight) + (SELECTION.red * (1F - weight)),
+                green = (base_color.green * weight) + (SELECTION.green * (1F - weight)),
+                blue = (base_color.blue * weight) + (SELECTION.blue * (1F - weight)),
+            )
+        }
+    }
+
+    val SELECTED_SECONDARY: (Boolean, Color) -> Color = SELECTED_PRIMARY
+
+    val MUTED: (Boolean, Color) -> Color = { is_empty, line_color ->
+        val (grey, weight) = if (is_empty) {
+            Pair(.5F, .5F)
+        } else {
+            Pair(line_color.avg(), .3F)
+        }
+        Color(
+            red = (line_color.red * weight) + (grey * (1F - weight)),
+            green = (line_color.green * weight) + (grey * (1F - weight)),
+            blue = (line_color.blue * weight) + (grey * (1F - weight)),
+            line_color.alpha
+        )
+    }
+
     val LEAF_COLOR_INVALID = Color(0xFFe51C3A)
     val LEAF_COLOR_INVALID_SELECTED = Color(0xFF890E21)
 
-    val EFFECT_COLOR = Color(0xFFCB9C20)
-    val EFFECT_SELECTION = Color(0xFF16500F)
-    val EFFECT_COLOR_SECONDARY = Color(0xFF567B80)
     val EFFECT_LINE_COLOR = Color(0xFFFFFFFF)
     val EFFECT_LINE_COLOR_NIGHT = Color(0xFF000000)
 
-    val SELECTION = Color(0xFF0033AA)
-    val LINE_COLOR = Color(0xFFE0E0E0)
-    val LINE_SELECTED = Color(0xFF5BA1D6)
-    val LINE_COLOR_NIGHT = Color(0xFF232323)
-    val LINE_COLOR_SECONDARY = Color(0xFF95BFDE)
-    val LINE_COLOR_SECONDARY_NIGHT = Color(0xFF416B8B)
-
-    val MUTED_LEAF_COLOR = Color(0xFF888888)
-    val MUTED_LEAF_SELECTED = Color(0xFF444444)
-    val MUTED_LINE_SELECTED = Color(0xFF727272)
-    val MUTED_LINE_COLOR = Color(0xFF9D9D9D)
-    val MUTED_SECONDARY = Color(0xFF929292)
-
-    val MUTED_LEAF_COLOR_NIGHT = Color(0xFF5E5E5E)
-    val MUTED_LEAF_SELECTED_NIGHT = Color(0xFFA9A9A9)
-    val MUTED_LINE_SELECTED_NIGHT = Color(0xFF838383)
-    val MUTED_LINE_COLOR_NIGHT = Color(0xFF454545)
-    val MUTED_SECONDARY_NIGHT = Color(0xFF929292)
 
     enum class LeafState {
         Active,
@@ -70,140 +107,48 @@ object Colors {
         is_muted: Boolean,
         dark_mode: Boolean = false
     ): Pair<Color, Color> {
-        val event_color_base = if (is_muted) {
-            if (dark_mode) {
-                MUTED_LEAF_COLOR_NIGHT
-            } else {
-                MUTED_LEAF_COLOR
-            }
+        val (event_color, line_color) = if (is_effect_line) {
+            Pair(
+                line_palette.effect ?: channel_palette.effect ?: EFFECT_COLOR,
+                line_palette.effect_bg ?: channel_palette.effect_bg ?: if (dark_mode) EFFECT_LINE_COLOR_NIGHT else EFFECT_LINE_COLOR
+            )
         } else {
-            line_palette.event ?: channel_palette.event ?: LEAF_COLOR
-        }
-
-        val effect_color_base = if (is_muted) {
-            if (dark_mode) {
-                MUTED_LEAF_COLOR_NIGHT
-            } else {
-                MUTED_LEAF_COLOR
-            }
-        } else {
-            EFFECT_COLOR
-        }
-
-        // val event_bg_color_base = line_palette.event_bg ?: channel_palette.event_bg ?: if (dark_mode) LINE_COLOR_NIGHT else LINE_COLOR
-        // val effect_bg_color_base = line_palette.effect_bg ?: channel_palette.effect_bg ?: if (dark_mode) EFFECT_LINE_COLOR_NIGHT else EFFECT_LINE_COLOR
-
-        var primary_base = if (is_effect_line) {
-            when (active) {
-                LeafState.Active -> effect_color_base
-                LeafState.Spill -> {
-                    if ((effect_color_base.red + effect_color_base.green + effect_color_base.blue) / 3F > .5F) {
-                        Color(
-                            red = effect_color_base.red * .75F,
-                            green = effect_color_base.green * .75F,
-                            blue = effect_color_base.blue * .75F,
-                            alpha = effect_color_base.alpha
-                        )
-                    } else {
-                        Color(
-                            red = min(1F, effect_color_base.red / .75F),
-                            green = min(1F, effect_color_base.green / .75F),
-                            blue = min(1F, effect_color_base.blue / .75F),
-                            alpha = effect_color_base.alpha
-                        )
-                    }
-                }
-                LeafState.Empty -> null
-            }
-        } else {
-            when (active) {
-                LeafState.Active -> event_color_base
-                LeafState.Spill -> {
-                    Color(
-                        red = event_color_base.red * .75F,
-                        green = event_color_base.green * .75F,
-                        blue = event_color_base.blue * .75F,
-                        alpha = event_color_base.alpha
-                    )
-                }
-                LeafState.Empty -> null
-            }
-        }
-
-        // Null implies empty, we don't want to apply tint to empty leafs
-        if (primary_base == null) {
-            primary_base = if (!is_muted) {
-                when (selected) {
-                    LeafSelection.Primary -> LINE_SELECTED
-                    LeafSelection.Secondary -> {
-                        if (dark_mode) {
-                            LINE_COLOR_SECONDARY_NIGHT
-                        } else {
-                            LINE_COLOR_SECONDARY
-                        }
-                    }
-                    LeafSelection.Unselected -> {
-                        if (dark_mode) {
-                            if (is_effect_line) {
-                                EFFECT_LINE_COLOR_NIGHT
-                            } else {
-                                LINE_COLOR_NIGHT
-                            }
-                        } else if (is_effect_line) {
-                            EFFECT_LINE_COLOR
-                        } else {
-                            LINE_COLOR
-                        }
-                    }
-                }
-            } else if (!dark_mode)  {
-                when (selected) {
-                    LeafSelection.Secondary,
-                    LeafSelection.Primary -> MUTED_LINE_SELECTED
-                    LeafSelection.Unselected -> MUTED_LINE_COLOR
-                }
-            } else {
-                when (selected) {
-                    LeafSelection.Secondary,
-                    LeafSelection.Primary -> MUTED_LINE_SELECTED_NIGHT
-                    LeafSelection.Unselected -> MUTED_LINE_COLOR_NIGHT
-                }
-            }
-        } else {
-            when (selected) {
-                LeafSelection.Unselected -> {}
-                else -> {
-                    val weight = .5F
-                    primary_base = if (is_effect_line) {
-                        Color(
-                            red = ((primary_base.red * .8F) * weight) + (EFFECT_SELECTION.red * (1F - weight)),
-                            green = ((primary_base.green * .8F) * weight) + (EFFECT_SELECTION.green * (1F - weight)),
-                            blue = ((primary_base.blue * .8F) * weight) + (EFFECT_SELECTION.blue * (1F - weight)),
-                        )
-                    } else {
-                        Color(
-                            red = ((primary_base.red * .8F) * weight) + (SELECTION.red * (1F - weight)),
-                            green = ((primary_base.green * .8F) * weight) + (SELECTION.green * (1F - weight)),
-                            blue = ((primary_base.blue * .8F) * weight) + (SELECTION.blue * (1F - weight)),
-                        )
-                    }
-                }
-            }
-        }
-
-        if (is_muted) {
-            val avg = (primary_base.red + primary_base.green + primary_base.blue) / 3F
-            primary_base = Color(
-                red = avg,
-                green = avg,
-                blue = avg,
-                alpha = primary_base.alpha
+            Pair(
+                line_palette.event ?: channel_palette.event ?: LEAF_COLOR,
+                line_palette.event_bg ?: channel_palette.event_bg ?: if (dark_mode) LINE_COLOR_NIGHT else LINE_COLOR
             )
         }
 
+        val is_empty = active == LeafState.Empty
+
+        var leaf_color = when(active) {
+            LeafState.Active -> event_color
+            LeafState.Spill -> SPILL(event_color)
+            LeafState.Empty -> line_color
+        }
+
+
+        leaf_color = when(selected) {
+            LeafSelection.Primary -> SELECTED_PRIMARY(is_empty, leaf_color)
+            LeafSelection.Secondary -> SELECTED_SECONDARY(is_empty, leaf_color)
+            LeafSelection.Unselected -> leaf_color
+        }
+
+        if (is_muted) {
+            leaf_color = MUTED(is_empty, leaf_color)
+        }
+
         return Pair(
-            primary_base,
-            this.get_text(primary_base)
+            leaf_color,
+            this.get_text(leaf_color)
         )
     }
+}
+
+fun Color.avg(): Float {
+    return (this.red + this.green + this.blue) / 3
+}
+fun Color.grey(): Color {
+    val avg = this.avg()
+    return Color(avg, avg,avg, this.alpha)
 }
